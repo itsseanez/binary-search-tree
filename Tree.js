26532bc3d315e6c301c6f09b2525842366113795
itsseanez <74938477+itsseanez@users.noreply.github.com>
2023-11-28 17:20:14 -0500

    Insert new nodes into an existing tree

--- Tree.js
+++ Tree.js
@@ -2,12 +2,8 @@ const createNode = require('./Node');
 
 const Tree = (array) => {
     //Sorts and makes array unique
-    let sortedArray= array.sort();
+    let sortedArray = array.sort((a, b) => a - b);
     let uniqueArray= [...new Set(sortedArray)];
-
-    //Initialize start and end
-    let start= 0;
-    let end= uniqueArray.length-1
 
     //finds the root node of an array and builds a treee
     const buildTree = (array, start, end) => {
@@ -24,7 +20,36 @@ const Tree = (array) => {
     }
 
     //Initialize root node
-    let rootNode= buildTree(uniqueArray, start, end);
+    let rootNode= buildTree(uniqueArray, 0, uniqueArray.length-1);
+
+    //Insert new node into the tree
+    const insertNode = (value, pointer = rootNode) => {
+        if (value < pointer.value) {
+            if (pointer.leftChild === null) {
+                pointer.leftChild = createNode(value);
+                prettyPrint(rootNode);
+            } else {
+                insertNode(value, pointer.leftChild);
+            }
+
+        } else if(value > pointer.value) {
+            if (pointer.rightChild === null) {
+                pointer.rightChild = createNode(value);
+                prettyPrint(rootNode);
+            } else {
+                insertNode(value, pointer.rightChild);
+            }
+
+        } else {
+            createNode(value)
+        }
+
+    };
+
+
+    const deleteNode = (value, pointer = rootNode) => {
+        
+    };
 
     //Binary Search tree visualization
     const prettyPrint = (node, prefix = "", isLeft = true) => {
@@ -43,8 +68,9 @@ const Tree = (array) => {
     // Print the tree
     prettyPrint(rootNode);
 
-    return rootNode;
+    return {rootNode, insertNode, deleteNode};
 };
 
 // Example usage
-const myTree = Tree([4, 2, 6, 1, 3, 5, 7]);
+const myTree = Tree([1, 2, 3]);
+myTree.insertNode(8);
